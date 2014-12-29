c9b9bbc31cd60792d3843767df87bded37a8d14d
Thomas Clayton <dementik@gmail.com>
2014-12-29 10:29:22 +0200

    Modified look of the logbook. Prettified date (needs to be moved to global helpers at some point).

--- client/logbook/logbook.js
+++ client/logbook/logbook.js
@@ -1,6 +1,9 @@
 Template.logBook.helpers({
   jumps: function() {
    return Logbook.find({},{sort: {jumpnumber: -1}});
+  },
+  prettifyDate: function(timestamp) {
+    return moment(new Date(timestamp)).format('DD.MM.YYYY (dddd)');
   }
 });
 
