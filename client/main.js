bf35b819689cc80c1d668461d23a3ddf4e85f446
Thomas Clayton <dementik@gmail.com>
2014-12-31 19:05:56 +0200

    Added tts functionality.

--- client/main.js
+++ client/main.js
@@ -8,3 +8,17 @@ Meteor.subscribe('logbook');
 Accounts.ui.config({
   passwordSignupFields: "USERNAME_ONLY"
 });
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
+
+UI.registerHelper('say', function (){
+	var saytext = '';
+	for (i = 0;i<arguments.length;i++){
+	  if (typeof arguments[i] == 'string' || arguments[i] instanceof String) saytext=saytext + ' ' + arguments[i]
+	}
+	tts.speak(saytext,'fi');
+ });
