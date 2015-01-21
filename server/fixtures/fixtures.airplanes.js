8ba7200cec8b8a52345e777e7b6073c4e99aafa3
Thomas Clayton <dementik@gmail.com>
2015-01-22 00:44:53 +0200

    Updated meteor version, removed Pilatus from fixtures

--- server/fixtures/fixtures.airplanes.js
+++ server/fixtures/fixtures.airplanes.js
@@ -13,14 +13,6 @@ if (Airplanes.find().count() === 0) {
     model: '172',
     mtow: '1100',
     maxaltitude: '2700',
-    maxjumpers: 4
-  });
-  Airplanes.insert({
-    registration: 'OH-CEW',
-    make: 'Pilatus',
-    model: 'Porter',
-    mtow: '2000',
-    maxaltitude: '4000',
-    maxjumpers: 8
+    maxjumpers: 3
   });
 }
