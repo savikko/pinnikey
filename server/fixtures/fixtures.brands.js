40693877bd735985c580c55a404bd572528f4fe1
Alexandre Pirojenko <Alexandre.Pirojenko@gmail.com>
2015-01-25 12:09:19 +0200

    no message

--- server/fixtures/fixtures.brands.js
+++ server/fixtures/fixtures.brands.js
@@ -1,33 +1,5 @@
 if (Brands.find().count() === 0) {
 
-//   Brands.insert({
-//     name: 'Sunrise Manufacturing International Inc.',
-//     url: "http://www.skydivewings.com/"
-//   });
-
-//   Brands.insert({
-//     name: 'United Parachute Technologies',
-//     url: "http://www.unitedparachutetechnologies.com/"
-//   });
-
-//   Brands.insert({
-//     name: 'Sky Wide Systems',
-//     url: "http://sws.aero/",
-//     established: new Date('2006','11')
-//   });
-
-//   Brands.insert({
-//     name: 'Sun Path Products, Inc.',
-//     url: "http://www.sunpath.com/"
-//   });
-
-//   Brands.insert({
-//     name: 'Paratec GmbH',
-//     url: "http://www.paratec.de/"
-//   });
-
-
-
   Brands.insert({ name : "United Parachute Technologies", url : "http://www.unitedparachutetechnologies.com/"});
   Brands.insert({ name : "Sky Wide Systems", url : "http://sws.aero/", established : new Date('2006','11')});
   Brands.insert({ name : "Sun Path Products, Inc.", url : "http://www.sunpath.com/"});
