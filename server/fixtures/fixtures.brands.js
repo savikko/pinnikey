a0a36ef9358fe6aad51e7d35b8576df4ea5e4ea6
Alexandre Pirojenko <Alexandre.Pirojenko@gmail.com>
2015-01-25 12:07:41 +0200

    Brands fixtures modified

--- server/fixtures/fixtures.brands.js
+++ server/fixtures/fixtures.brands.js
@@ -1,30 +1,41 @@
 if (Brands.find().count() === 0) {
 
-  Brands.insert({
-    name: 'Sunrise Manufacturing International Inc.',
-    url: "http://www.skydivewings.com/"
-  });
+//   Brands.insert({
+//     name: 'Sunrise Manufacturing International Inc.',
+//     url: "http://www.skydivewings.com/"
+//   });
 
-  Brands.insert({
-    name: 'United Parachute Technologies',
-    url: "http://www.unitedparachutetechnologies.com/"
-  });
+//   Brands.insert({
+//     name: 'United Parachute Technologies',
+//     url: "http://www.unitedparachutetechnologies.com/"
+//   });
 
-  Brands.insert({
-    name: 'Sky Wide Systems',
-    url: "http://sws.aero/",
-    established: new Date('2006','11')
-  });
+//   Brands.insert({
+//     name: 'Sky Wide Systems',
+//     url: "http://sws.aero/",
+//     established: new Date('2006','11')
+//   });
 
-  Brands.insert({
-    name: 'Sun Path Products, Inc.',
-    url: "http://www.sunpath.com/"
-  });
+//   Brands.insert({
+//     name: 'Sun Path Products, Inc.',
+//     url: "http://www.sunpath.com/"
+//   });
 
-  Brands.insert({
-    name: 'Paratec GmbH',
-    url: "http://www.paratec.de/"
-  });
+//   Brands.insert({
+//     name: 'Paratec GmbH',
+//     url: "http://www.paratec.de/"
+//   });
+
+
+
+  Brands.insert({ name : "United Parachute Technologies", url : "http://www.unitedparachutetechnologies.com/"});
+  Brands.insert({ name : "Sky Wide Systems", url : "http://sws.aero/", established : new Date('2006','11')});
+  Brands.insert({ name : "Sun Path Products, Inc.", url : "http://www.sunpath.com/"});
+  Brands.insert({ name : "Paratec GmbH", url : "http://www.paratec.de/"});
+  Brands.insert({ name : "LARSEN & BRUSGAARD", url : "http://www.l-and-b.dk/about/history", established : new Date('1994')});
+  Brands.insert({ name : "Airtec GmbH", url : "http://www.cypres.cc/"});
+  Brands.insert({ name : "ICARUS Canopies", url : "http://www.icaruscanopies.aero/", established : new Date('1999')});
+  Brands.insert({ name : "MIRAGE SYSTEMS, INC.", url : "https://www.miragesys.com/", established : new Date('2004')});
+  Brands.insert({ name : "Sunrise Manufacturing International Inc.", url : "http://www.skydivewings.com/"});
 
 }
-
