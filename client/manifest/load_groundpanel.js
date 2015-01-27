56cc89e5b6b097b8d9c2f212ed8c4528eb188760
Thomas Clayton <dementik@gmail.com>
2015-01-27 18:34:30 +0200

    Added phonetized aircraftcall PoC

--- client/manifest/load_groundpanel.js
+++ client/manifest/load_groundpanel.js
@@ -5,18 +5,29 @@ Template.groundPanel.events({
         load = this; // load object for subfunctions
         status = $(event)[0].target.attributes.status.value; // is there some more sophisticated way to get status value from button?
         aircraftcall = Aircrafts.findOne(this.aircraft).registration.slice(-2).split('').join('');
+        // Phoneticize  -> needs to be moved to it's own function
+        var phoneticAc = '';
+        for (var i = 0, len = aircraftcall.length; i < len; i++) {
+            var alphabets = new Array("Alpha","Bravo","Charlie","Delta","Echo","Foxtrot","Golf","Hotel","India","Juliet","Kilo","Lima","Mike","November","Oscar","Papa","Quebec","Romeo","Sierra","Tango","Uniform","Victor","Whiskey","X-Ray","Yankee","Zulu");
+            var letter = aircraftcall[i].toUpperCase();      
+            var whatChar = letter.charCodeAt(0);
+              if ((whatChar > 64)&&(whatChar < 91)) {
+                  phonetic = whatChar-65;        
+                  phoneticAc = phoneticAc + ' ' + alphabets[phonetic];
+              } else
+              phoneticAc = phoneticAc + ' ' + letter;    
+        };
         Meteor.call("loadStatus", loadid, status,function(error,result){
             if(error){
               console.log(error.reason);
             }
             else{
               calltext='';
-              console.log(call);
               if (status=="call20min") { calltext='20 minutes.'; }
               if (status=="call10min") { calltext='10 minutes.'; }
               if (status=="call5min") { calltext='5 minutes. Gear up.'; }
               if (status=="callGo") { calltext=' go to the plane.'; }
-              if (calltext!="") { tts.speak('Load ' + load.loadnumber + ', ' + aircraftcall + ', ' + calltext,'en'); }   // do something with result
+              if (calltext!=="") { tts.speak('Load ' + load.loadnumber + ', ' + phoneticAc + ', ' + calltext,'en'); }
             }
         });
     }
