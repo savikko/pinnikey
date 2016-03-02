bd7d0145007801d11c7f3fc2e605323a00b3b608
Tommi Savikko <tommi.savikko@skydivejkl.fi>
2016-03-02 17:21:07 +0200

    weight and balance changes

--- server/fixtures/fixtures.aircrafts.js
+++ server/fixtures/fixtures.aircrafts.js
@@ -5,7 +5,13 @@ if (Aircrafts.find().count() === 0) {
     model: '182',
     mtow: '1338',
     maxaltitude: '4000',
-    maxjumpers: 4
+    maxjumpers: 4,
+    typeOfFuel: 'JET',
+    noOfPilots: 1,
+    minJumpers: 3,
+    maxJumpers: 5,
+    usableWeightForSkydivers: 800,
+    maxAltitude: 4000
   });
   Aircrafts.insert({
     registration: 'OH-CEW',
@@ -13,6 +19,12 @@ if (Aircrafts.find().count() === 0) {
     model: '172',
     mtow: '1100',
     maxaltitude: '2700',
-    maxjumpers: 3
+    maxjumpers: 3,
+    typeOfFuel: 'AVGAS',
+    noOfPilots: 1,
+    minJumpers: 3,
+    maxJumpers: 3,
+    usableWeightForSkydivers: 600,
+    maxAltitude: 2700
   });
 }
